30761cd2196357a492ce7ab91c10b2d023fe0d0a
Chakradhar Ananthoju <110176030+ChakradharAnanthoju@users.noreply.github.com>
2022-07-28 19:00:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-Streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Fruits, Oats, Dryfruits, Milk, Bread')
 
