657cef7a76fccb1acd8372f228470fc4c9d78865
Chakradhar Ananthoju <110176030+ChakradharAnanthoju@users.noreply.github.com>
2022-07-28 19:19:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ streamlit.title('Breakfast Favorites')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-:egg: streamlit.text('Hard-Boiled Free-Range Egg')
-<script src="https://gist.github.com/rxaviers/7360908.js"></script>
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
